0f82ff63e60739b78a11e54fb1e55191355d035d
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-06-25 14:37:22 -0600

    add an attribute, add some default values

--- customer/models.py
+++ customer/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
-    dui = models.CharField("DUI", max_length=10, unique=True, blank=True, null=True)
+    dui = models.CharField("DUI", max_length=10, unique=True, blank=True, null=True, default="00000000-0")
     firstName = models.CharField("Nombre", max_length=255)
     lastName = models.CharField("Apellido", max_length=255)
-    email = models.EmailField("Correo electrónico", unique=True, blank=True, null=True)
-    number = models.CharField("Número de teléfono", max_length=20, blank=True, null=True)
+    email = models.EmailField("Correo electrónico", unique=True, blank=True, null=True, default="cliente@example.com")
+    number = models.CharField("Número de teléfono", max_length=20, blank=True, null=True, default="0000-0000")
     created_at = models.DateTimeField("Fecha de creación", auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Creado por")
+    edited_at = models.DateTimeField("Fecha de edición", auto_now=True)
 
     def __str__(self):
         return f"{self.firstName} {self.lastName}"
