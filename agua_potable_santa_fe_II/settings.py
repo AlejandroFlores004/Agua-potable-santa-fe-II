c2881a8a8c59a3e608d54b19b1fbc856eb312753
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-06-27 14:39:29 -0600

    model for fee and test in admin

--- agua_potable_santa_fe_II/settings.py
+++ agua_potable_santa_fe_II/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'customer',
     'connection',
+    'billing',
 ]
 
 MIDDLEWARE = [
--- billing/admin.py
+++ billing/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from billing.models import Fee
+
 # Register your models here.
+class FeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount', 'created_at', 'created_by')
+    search_fields = ('name', 'amount', 'created_by__username')
+    list_filter = ('created_at',)
+
+admin.site.register(Fee, FeeAdmin)
