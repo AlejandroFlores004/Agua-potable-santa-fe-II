a67028d1a600cc4e9bda6378c7c5b4712a600e01
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-06-27 11:23:41 -0600

    Making the model and testing in admin

--- connection/admin.py
+++ connection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from connection.models import Line
 
 # Register your models here.
+
+
+class LineAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'customer', 'created_at', 'updated_at')
+    search_fields = ('code', 'description', 'customer__firstName', 'customer__lastName')
+    list_filter = ('created_at', 'updated_at', 'customer')
+    readonly_fields = ('created_at', 'updated_at', 'code')
+
+admin.site.register(Line, LineAdmin)
