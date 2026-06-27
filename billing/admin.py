75fc823e168c6c161d5111cbafef0a168622557a
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-06-27 15:10:49 -0600

    model of invoce line added and test in admin

--- billing/admin.py
+++ billing/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from billing.models import Fee, Invoice
+from billing.models import Fee, Invoice, InvoiceLine
 
 # Register your models here.
 class FeeAdmin(admin.ModelAdmin):
@@ -16,3 +16,12 @@ class InvoiceAdmin(admin.ModelAdmin):
     list_filter = ('start_date', 'end_date', 'due_date', 'created_at')
 
 admin.site.register(Invoice, InvoiceAdmin)
+
+class InvoiceLineAdmin(admin.ModelAdmin):
+    list_display = ('code', 'invoice', 'line', 'isPaid', 'created_at', 'created_by')
+    search_fields = ('code', 'invoice__fee__name', 'line__code', 'line__customer__firstName', 'line__customer__lastName', 'created_by__username')
+    list_filter = ('isPaid', 'created_at')
+    readonly_fields = ('code', 'created_at')
+
+admin.site.register(InvoiceLine, InvoiceLineAdmin)
+    
