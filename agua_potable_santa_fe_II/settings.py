572ee74d311c80026fd3bade79a5f3dff2c23292
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-06-27 15:18:36 -0600

    Everything to start with tailwind

--- agua_potable_santa_fe_II/settings.py
+++ agua_potable_santa_fe_II/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
     'customer',
     'connection',
     'billing',
@@ -118,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Tailwind CSS configuration
+TAILWIND_APP_NAME = 'theme'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
