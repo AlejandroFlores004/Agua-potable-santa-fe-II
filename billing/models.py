2ac2867307271cdbfc51e8b36d538d3588732c71
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-06-27 15:10:49 -0600

    model of invoce line added and test in admin

--- billing/models.py
+++ billing/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from connection.models import Line
+from datetime import datetime
 
 # Create your models here.
 class Fee(models.Model):
@@ -30,3 +32,29 @@ class Invoice(models.Model):
     class Meta:
         verbose_name = "Factura"
         verbose_name_plural = "Facturas"
+
+class InvoiceLine(models.Model):
+    code = models.CharField(max_length=10, unique=True, blank=True, null=True)
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    line = models.ForeignKey(Line, on_delete=models.CASCADE)
+    isPaid = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Factura de linea: {self.code} - Linea: {self.line.code} - cliente: {self.line.customer.firstName} {self.line.customer.lastName} - Pagada: {self.isPaid}"
+    
+    class Meta:
+        verbose_name = "Factura de Linea"
+        verbose_name_plural = "Facturas de Lineas"
+
+    
+    def code_generator(self):
+        return f"{datetime.now().year}-{self.line.code}-{self.id}"
+    
+    def save(self, *args, **kwargs):
+        if not self.code:
+            super().save(*args, **kwargs)  # Save the instance to get an ID
+            self.code = self.code_generator()
+        super().save(*args, **kwargs)  # Save again to update the code
