f262892a5d6989b9b27b6a244800cd2b8eb80a07
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-13 11:06:57 -0600

    Add customer module with home view and navigation links

--- agua_potable_santa_fe_II/urls.py
+++ agua_potable_santa_fe_II/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shell.urls')),
+    path('customer/', include('customer.urls')),
 ]
 
 if settings.DEBUG:
