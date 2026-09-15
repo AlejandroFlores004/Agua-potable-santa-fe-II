d790dc4d197b026969c2c4ae02d871e7ca84726b
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-09-15 17:27:02 -0600

    Update LineAdmin list_filter and change Docker port mapping

--- connection/admin.py
+++ connection/admin.py
@@ -7,7 +7,7 @@ from connection.models import Line
 class LineAdmin(admin.ModelAdmin):
     list_display = ('code', 'description', 'customer', 'created_at', 'updated_at')
     search_fields = ('code', 'description', 'customer__firstName', 'customer__lastName')
-    list_filter = ('created_at', 'updated_at', 'customer')
+    list_filter = ('created_at', 'updated_at', 'description')
     readonly_fields = ('created_at', 'updated_at', 'code')
 
 admin.site.register(Line, LineAdmin)
